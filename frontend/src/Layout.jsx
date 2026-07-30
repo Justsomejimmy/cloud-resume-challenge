b63c9cceb7fd968f443198a675672e461a56e2d2
Justsomejimmy <jxnzp1@gmail.com>
2026-07-30 21:31:28 +0000

    Added page locations to project

--- frontend/src/Layout.jsx
+++ frontend/src/Layout.jsx
@@ -1,9 +1,33 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Header from 'components/Header'
-import { Outlet } from 'react-router'
+import { Outlet, NavLink, useLocation } from 'react-router'
 
 export default function Layout() {
-  const [count, setCount] = useState(0)
+  const location = useLocation();
+  useEffect(() => {
+  
+  let pageName = ''
+  if (location.pathname === "/") {
+    pageName = 'home'
+  }
+  else if (location.pathname === "/resume") {
+    pageName = 'resume'
+  }
+  else if (location.pathname === "/projects") {
+    pageName = 'projcects'
+  }
+  else if (location.pathname === "/aboutme") {
+    pageName = 'aboutme'
+  }
+
+  document.body.setAttribute("location", pageName);
+  document.body.classname = `${pageName}`;
+
+  return () => {
+    document.body.removeAttribute("location");
+    document.body.className = "";
+  };
+ }, [location]);
 
   return (
     <>
