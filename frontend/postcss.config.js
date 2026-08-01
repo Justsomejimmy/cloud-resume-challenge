6c780280825f2fc87ef41ad15137ca0b422dadc4
Justsomejimmy <jxnzp1@gmail.com>
2026-08-01 08:11:10 +0000

    Added basic mobile styling with breakpoints and postcss

---
+++ frontend/postcss.config.js
@@ -0,0 +1,11 @@
+export default {
+    plugins: {
+        'postcss-preset-env': {
+            stage: 1,
+            features: {
+                'custom-media-queries': true,
+                'nesting-rules': true
+            }
+        }
+    }
+};
