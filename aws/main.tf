430fe9ad2835aba22cd1cd1cb707830ffbedbd4e
Justsomejimmy <jxnzp1@gmail.com>
2026-08-17 04:03:26 +0000

    Updated Terraform configuration files to include ACM certificate

--- aws/main.tf
+++ aws/main.tf
@@ -80,6 +80,7 @@ resource "aws_cloudfront_distribution" "portfolio" {
   enabled = true
 
   default_root_object = "index.html"
+  aliases             = [var.domain_name]
 
   origin {
     domain_name              = aws_s3_bucket.portfolio.bucket_regional_domain_name
@@ -118,7 +119,9 @@ resource "aws_cloudfront_distribution" "portfolio" {
   }
 
   viewer_certificate {
-    cloudfront_default_certificate = true
+    acm_certificate_arn      = var.acm_certificate_arn
+    ssl_support_method       = "sni-only"
+    minimum_protocol_version = "TLSv1.2_2021"
   }
 }
 
--- aws/variables.tf
+++ aws/variables.tf
@@ -2,3 +2,14 @@ variable "bucket_name" {
   description = "Globally unique name for the portfolio S3 bucket"
   type        = string
 }
+
+variable "domain_name" {
+  description = "Custom domain for the portfolio"
+  type        = string
+  default     = "jimmyhoangresume.com"
+}
+
+variable "acm_certificate_arn" {
+  description = "ARN of the ACM certificate for CloudFront"
+  type        = string
+}
